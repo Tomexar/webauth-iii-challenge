d44c2b6c9dd964e10f4c96631a2ef75fd96b4e1f
Tomexar <tommyexar@gmail.com>
2019-06-12 16:18:19 -0600

    backend for part 1

---
+++ data/seeds/001.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          username: 'Tom',
+          password: 'test',
+          department:'Lambda'
+        },
+      
+      ]);
+    });
+};
